06fd2951ba1f4042eb646ae531c3f684e0f3e2c6
Nick's_github <smilexd7@naver.com>
2026-06-15 20:55:06 +0900

    First Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int hp = 80;
+string name = "동훈";
+
+// System.Console.WriteLine(hp);
+// System.Console.WriteLine(name);
+
+// Console.WriteLine($"체력: {hp}");
+
+float speed = 5.5f;
+bool isSurvive = true;
+
+Console.WriteLine($"이름: {name}");
+Console.WriteLine($"체력: {hp}");
+Console.WriteLine($"이동속도: {speed}");
+Console.WriteLine($"생존: {isSurvive}");
